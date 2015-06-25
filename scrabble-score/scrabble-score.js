2f1c46b8fb64f588101c34aab5d3d506bca448ff
Matthew Morgan <matt@dulcetsoftware.com>
2015-06-25 06:39:18 -0400

    Update scrabble-score.js

--- scrabble-score/scrabble-score.js
+++ scrabble-score/scrabble-score.js
@@ -1,21 +1,10 @@
 //score is 1 unless specified
-var SCORE = {
-	D : 2,
-	G : 2,
-	B : 3,
-	C : 3,
-	M : 3,
-	P : 3,
-	F : 4,
-	H : 4,
-	V : 4,
-	W : 4,
-	Y : 4,
+var SCORE = {D : 2,G : 2,
+	B : 3, C : 3, M : 3, P : 3,
+	F : 4, H : 4, V : 4, W : 4, Y : 4, 
 	K : 5,
-	J : 8,
-	X : 8,
-	Q : 10,
-	Z : 10
+	J : 8, X : 8,
+	Q : 10, Z : 10
 };
 
 var Scrabble = module.exports = function(word){
