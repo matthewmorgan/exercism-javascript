5be9b587a77c27764a98fecac1e477400bdd90c5
Matthew Morgan <matt@dulcetsoftware.com>
2015-08-09 15:07:15 -0400

    cleanup

--- robot-name/robot-name.js
+++ robot-name/robot-name.js
@@ -1,32 +1,30 @@
-var usedNames=[];
+var used=[];
 
-module.exports = Robot = function(){
+module.exports = function(){
 
 	var getRandomInt = function(min, max) {
 		return Math.floor(Math.random() * (max - min)) + min;
 	};
+
 	var getRandomLetter = function(){
 		return String.fromCharCode(getRandomInt(65,90));
 	};
+
 	var generateName = function(){
 		var temp=getRandomLetter()+getRandomLetter();
 		temp+=getRandomInt(0,9);
 		temp+=getRandomInt(0,9);
 		temp+=getRandomInt(0,9);
-		while (usedNames[temp]){
-			temp=generateName();
-		}
-		usedNames.push(temp);
+		while (used[temp]){ temp=generateName(); }
+		used.push(temp);
 		return temp;
 	};
 
 	var name=generateName();
 
 	return {
-		reset: function(){
-			this.name=generateName();
-		},
+		reset: function(){ this.name=generateName(); },
 		name: name,
-		usedNames: usedNames
+		usedNames: used
 	}
 };
