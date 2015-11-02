10c6adcf2d6f98d2677955dd9566415be545bf0c
Matthew Morgan <matt@dulcetsoftware.com>
2015-11-02 09:06:23 -0500

    grains new test

--- grains/grains.js
+++ grains/grains.js
@@ -1,6 +1,6 @@
 module.exports=function(){
-	return {
-		square: function(n){ return Math.pow(2, n-1) },
-		total: function(){ return Math.pow(2, 64)-1 }
-	}
+  return {
+    square: function(n){ return Math.pow(2, n-1) },
+    total: function(){ return Math.pow(2, 64)-1 }
+  }
 };
